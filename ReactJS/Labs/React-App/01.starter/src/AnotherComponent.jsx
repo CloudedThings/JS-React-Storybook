a3dbd526bb49a8b1b017b8748d0d7635ae68fbd6
Leszek Ucinski <rahvinos@gmail.com>
2021-12-09 14:59:06 +0100

    cloud academy lab

---
+++ ReactJS/Labs/React-App/01.starter/src/AnotherComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AnotherComponent = () => {
+    return (
+        <>
+            <p>Some text</p>
+            <p>More text</p>
+        </>
+    )
+};
+
+export default AnotherComponent;
---
+++ ReactJS/Labs/React-App/01.starter/src/MyClassComponent.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class MyClassComponent extends Component{
+    
+    render() {
+        return (
+            <>
+                <h2>MyClass Component</h2>
+                <p>With some content</p>
+            </>
+        );
+    }
+}
+
+export default MyClassComponent;
