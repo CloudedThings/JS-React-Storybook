1116829c294a74821a2e3d1d393279841083d249
Leszek Ucinski <rahvinos@gmail.com>
2022-05-19 16:25:44 +0200

    Add API routes

--- MERN/DevConnector/config/keys.js
+++ MERN/DevConnector/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://admin:NoweHaslo77@cluster0.cfd5n.mongodb.net/?retryWrites=true&w=majority'
-}
+  mongoURI: 'mongodb+srv://admin:NoweHaslo77@cluster0.cfd5n.mongodb.net/?retryWrites=true&w=majority',
+  secretOrKey: 'secret'
+};
