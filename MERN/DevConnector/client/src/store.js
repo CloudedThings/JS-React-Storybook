22b187e023677176e6abc59b274c5ff81c30e8de
Leszek Ucinski <rahvinos@gmail.com>
2022-05-24 15:39:41 +0200

    Start new React App

--- MERN/DevConnector/client/src/store.js
+++ MERN/DevConnector/client/src/store.js
@@ -1,8 +1,13 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import {
+  legacy_createStore as createStore,
+  applyMiddleware,
+  compose,
+} from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 const initialState = {};
+
 const middleware = [thunk];
 
 const store = createStore(
